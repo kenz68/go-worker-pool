79c6b39c10778a446bd81a206d1c8ade8b15b22a
quocle <quoc.le@futa.vn>
2025-02-19 16:03:15 +0700

    add wait group and handler errors

--- main.go
+++ main.go
@@ -2,30 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
+func worker(id int, wg *sync.WaitGroup, jobs <-chan int, results chan<- int, errors chan<- error) {
 	for j := range jobs {
 		fmt.Println("worker", id, "processing job", j)
 		time.Sleep(time.Second)
-		results <- j * 2
+		if j%2 == 0 {
+			results <- j * 2
+		} else {
+			errors <- fmt.Errorf("worker %d error on job: %d", id, j)
+		}
+		wg.Done()
 	}
 }
 
 func main() {
 	jobs := make(chan int, 100)
 	results := make(chan int, 100)
+	errors := make(chan error, 100)
+
+	var wg sync.WaitGroup
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+		go worker(w, &wg, jobs, results, errors)
 	}
 
 	for j := 1; j <= 9; j++ {
 		jobs <- j
+		wg.Add(1)
 	}
 	close(jobs)
+	wg.Wait()
 
-	for a := 1; a <= 9; a++ {
-		<-results
+	select {
+	case err := <-errors:
+		fmt.Println("finished with error:", err)
+	default:
 	}
 }
